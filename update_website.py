5117c615cbb7d07595ba31df7e162a6f07a3af37
twall <tomwallroth@gmail.com>
2013-11-02 15:34:24 +0100

    rewrote website creation script

--- update_website.py
+++ update_website.py
@@ -3,27 +3,47 @@ import os
 import subprocess
 import codecs
 import re
+import shutil
 
 RESOURCES = os.path.dirname(os.path.abspath(__file__))
-SOURCES = os.path.join(RESOURCES,'sources')
-SCREENSHOTDIR = 'screenshots'
+PATH_SOURCES = os.path.join(RESOURCES, 'cherrymusic_sources')
+GITPATH_SOURCES = os.path.join(PATH_SOURCES,'.git')
+PATH_WIKI = os.path.join(RESOURCES,'wiki')
+GITPATH_WIKI = os.path.join(PATH_WIKI,'.git')
+
+MAIN_TEMPLATE = os.path.join(RESOURCES, 'template.html')
+PAGES_SOURCE_PATH = os.path.join(RESOURCES, 'pages.src')
+DEPLOY_PATH = os.path.join(RESOURCES, 'deploy')
+ASSET_PATH =  os.path.join(RESOURCES, 'pages.assets')
+
+SCREENSHOTDIR = os.path.join(ASSET_PATH, 'screenshots')
 SCREENSHOTTHUMBS = os.path.join(SCREENSHOTDIR,'thumb')
 SCREENSHOTSABS = os.path.join(RESOURCES,SCREENSHOTDIR)
-if not os.path.exists(SOURCES):
-    print('Checking out latest source tree')
-    subprocess.check_output(['git', 'clone', 'git://github.com/devsnd/cherrymusic.git', SOURCES])
-GITPATH = os.path.join(SOURCES,'.git')
-VERSIONPATH = os.path.join(RESOURCES,'versions')
+
+VERSIONPATH = os.path.join(ASSET_PATH,'versions')
+
+INDEX = 'Home'
+
+if not os.path.exists(PATH_SOURCES):
+    print('Checking out latest source repo')
+    subprocess.check_output(['git', 'clone', 'git://github.com/devsnd/cherrymusic.git', PATH_SOURCES])
+if not os.path.exists(PATH_WIKI):
+    print('Checking out latest wiki repo')
+    subprocess.check_output(['git', 'clone', 'git://github.com/devsnd/cherrymusic.wiki.git', PATH_WIKI])
+if not os.path.exists(DEPLOY_PATH):
+    os.mkdir(DEPLOY_PATH)
 
 def utf8(x):
     return codecs.decode(x,'UTF-8')
 
 def downloadtags():
-    print('updating repo...')
-    print(utf8(subprocess.check_output(['git','--git-dir='+GITPATH, '--work-tree='+SOURCES, 'pull'])))
+    print('updating wiki repo...')
+    print(utf8(subprocess.check_output(['git','--git-dir='+GITPATH_WIKI, '--work-tree='+PATH_WIKI, 'pull'])))
+    print('updating sources repo...')
+    print(utf8(subprocess.check_output(['git','--git-dir='+GITPATH_SOURCES, '--work-tree='+PATH_SOURCES, 'pull'])))
     print('DOWNLOADING tagged master versions')
     print('Getting Version info...')
-    TAGS=subprocess.check_output(['git','--git-dir='+GITPATH, '--work-tree='+SOURCES, 'tag']).split()
+    TAGS=subprocess.check_output(['git','--git-dir='+GITPATH_SOURCES, '--work-tree='+PATH_SOURCES, 'tag']).split()
     TAGS=map(utf8,TAGS)
     if not os.path.exists(VERSIONPATH):
         os.makedirs(VERSIONPATH)
@@ -41,27 +61,52 @@ def downloadtags():
             except subprocess.CalledProcessError:
                 print('ERROR donwloading file! https://github.com/devsnd/cherrymusic/tarball/'+i)
                 exit(1)
-    print('adding all versions to repo...')
+    print('adding all master versions to website repo...')
     print(utf8(subprocess.check_output(['git','add',VERSIONPATH+'/*'])))
     print('done.')
-    
+
+def source_to_page_file_name(filename):
+    if source_to_page_title(filename) == INDEX:
+        return 'index.html'
+    return filename[filename.index('.')+1:]
+
+def source_to_page_title(filename):
+    return filename[filename.index('.')+1:filename.rindex('.')]
+
+def generateNavigation(filenames, active_file):
+    htmlli = ''
+    for filename in filenames:
+        active = 'active' if filename == active_file else ''
+        data = (active, source_to_page_file_name(filename), source_to_page_title(filename))
+        htmlli += '\t<li class="%s"><a data-toggle="tab" href="%s">%s</a></li>\n' % data
+    return '<ul class="nav nav-tabs" id="myTab">\n' + htmlli + '</ul>\n'
+
+
 def generateWebsite():
-    source_content = readFile('index.src.html')
-    source_content = generateDownload(source_content)
-    source_content = generateScreenshotSection(source_content)
-    source_content = generateChanges(source_content)
-    with open('index.html','w') as index:
-        index.write("""
-            <!---- DO NOT CHANGE THE INDEX FILE. 
-                    IT IS GENERATED AUTOMATICALLY.
-            ------>
-""")
-        index.write(source_content)
-    print('all done.')
-    
+    main_template = readFile(MAIN_TEMPLATE)
+    page_file_names = sorted(os.listdir(PAGES_SOURCE_PATH))
+    for page_file_name in page_file_names:
+        page_file_path = os.path.join(PAGES_SOURCE_PATH, page_file_name)
+        content = readFile(page_file_path)
+        if '<!--SCREENSHOT-SECTION-->' in content:
+            content = generateScreenshotSection(content)
+        if '<!--CHANGELOG-->' in content:
+            content = generateChanges(content)
+        if '<!--DOWNLOAD-PAGE-->' in content:
+            content = generateDownload(content)
+        nav = generateNavigation(page_file_names, page_file_name)
+        content = main_template.replace('<!--PAGE-CONTENT-->', content)
+        content = content.replace('<!--NAVIGATION-->', nav)
+        deploy_page_file_name = source_to_page_file_name(page_file_name)
+        writeFile(os.path.join(DEPLOY_PATH, deploy_page_file_name), content)
+
 def readFile(filename):
     with open(filename,'r') as fh:
         return fh.read()
+
+def writeFile(filename, content):
+    with open(filename, 'w') as outfh:
+        outfh.write(content)
 
 def generateDownload(content):
     get_version_by_filename = lambda x: re.findall('(\d\.\d+(\.\d+)?)',x)[0][0]
@@ -80,7 +125,8 @@ def generateScreenshotSection(content):
 
 def resizeImage(imagepath,size):
     with open(os.devnull,'w') as devnull:
-        cmd = ['convert',imagepath,'-resize',str(size[0])+'x'+str(size[1]),'png:-']
+        imgsize = str(size[0])+'x'+str(size[1])
+        cmd = ['convert', imagepath, '-resize', imgsize, 'png:-']
         im = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         data = im.communicate()[0]
         return data
@@ -114,7 +160,7 @@ def generateScreenshotList(imgsize):
 
 def generateChanges(content):
     ret = '<div class="accordion-group">'
-    with open(SOURCES+'/CHANGES','r') as changelog:
+    with open(PATH_SOURCES+'/CHANGES','r') as changelog:
         currentVersion = ''
         for line in changelog.readlines():
             if(line.startswith('0')):
@@ -137,6 +183,14 @@ def generateChanges(content):
     ret += '</div>'
     return content.replace('<!--CHANGELOG-->',ret)
 
+def copyAssets():
     
-downloadtags()
-generateWebsite()
+    
+
+if __name__ == '__main__':
+    downloadtags()
+    # remove old deployment
+    shutile.rmtree(DEPLOY_PATH)
+    # copy assets
+    shutil.copytree(ASSET_PATH, DEPLOY_PATH)
+    generateWebsite()
